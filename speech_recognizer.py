d8d4f21e7341b34f79b331a1a9c6496ee5296a3d
lbiagetti <lucasbiagetti@gmail.com>
2023-12-18 16:54:14 -0300

    speech recognizer v1

--- speech_recognizer.py
+++ speech_recognizer.py
@@ -1,18 +1,44 @@
-import transformers
+import io
+import speech_recognition as sr
+from pydub import AudioSegment
 
-model = transformers.AutoModelForSpeechRecognition.from_pretrained("facebook/wav2vec2-large-xlsr-53-german")
+class AudioTranscriberSingleton:
+    _instance = None
 
-# Cargar el archivo de audio
-wav = librosa.load("/home/lucasbiagetti/Documentos/gitPr/speechRecognitionTelegram/voice_messages/pruebawav.wav")
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(AudioTranscriberSingleton, cls).__new__(cls)
+            cls._instance._recognizer = sr.Recognizer()  # Create recognizer instance
+            # Add any other initialization code here
+        return cls._instance
 
-# Convertir el archivo de audio a un tensor
-audio_tensor = torch.tensor(wav)
+    def transcribe_audio(self, audio_file_path):
+        # Convert OGG to WAV using pydub
+        audio = AudioSegment.from_file(audio_file_path, format="ogg")
 
-# Desgrabar el archivo de audio
-transcript = model(audio_tensor).logits.argmax(dim=-1)
+        # Create a BytesIO object to hold the converted audio data
+        buffer = io.BytesIO()
 
-# Convertir la transcripción a texto
-transcript = transcript.cpu().numpy().tolist()
+        # Export the converted audio data to the BytesIO object
+        audio.export(buffer, format="wav")
 
-# Imprimir la transcripción
-print(transcript)
+        # Load the converted audio data from the BytesIO object
+        with sr.AudioFile(buffer) as audio_file:
+            # Adjust for ambient noise
+            self._recognizer.adjust_for_ambient_noise(audio_file)
+
+            # Listen to the audio and transcribe
+            try:
+                print("Transcribing audio...")
+                audio_data = self._recognizer.record(audio_file)
+                text = self._recognizer.recognize_google(audio_data, language="es-ES")
+                return text
+            except sr.UnknownValueError:
+                print("Google Speech Recognition could not understand the audio")
+            except sr.RequestError as e:
+                print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+
+# Example usage:
+audio_transcriber = AudioTranscriberSingleton()
+print(audio_transcriber.transcribe_audio('voice_messages/prueba.ogg'))
