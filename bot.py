52c32fdc6f1e47ba73b4f49137482889164e52fa
lbiagetti <lucasbiagetti@gmail.com>
2023-12-18 17:12:08 -0300

    speeche reconition with python v1

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from speech_recognizer import AudioTranscriberSingleton
 
-TOKEN = ''
+TOKEN = '6794189393:AAE6ZzH2wriksGIv7lvnwOi7odaKcjrZtB0'
+audio_transcriber = AudioTranscriberSingleton()
+
 
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Hola, soy tu bot de Telegram. ¡Envíame mensajes!')
@@ -15,9 +18,10 @@ def handle_voice(update: Update, context: CallbackContext) -> None:
     file_id = voice.file_id
 
     voice_file = context.bot.get_file(file_id)
-    voice_file.download(f"voice_messages/{file_id}.ogg")
+    voice_file.download("voice_messages/message.ogg")
+    text = audio_transcriber.transcribe_audio("voice_messages/message.ogg")
 
-    update.message.reply_text(f"Recibí un mensaje de voz de {duration} segundos. ID: {file_id}")
+    update.message.reply_text(f"Haz dicho {text}")
 
 def main() -> None:
     updater = Updater(TOKEN)
--- speech_recognizer.py
+++ speech_recognizer.py
@@ -9,25 +9,18 @@ class AudioTranscriberSingleton:
         if cls._instance is None:
             cls._instance = super(AudioTranscriberSingleton, cls).__new__(cls)
             cls._instance._recognizer = sr.Recognizer()  # Create recognizer instance
-            # Add any other initialization code here
         return cls._instance
 
     def transcribe_audio(self, audio_file_path):
-        # Convert OGG to WAV using pydub
         audio = AudioSegment.from_file(audio_file_path, format="ogg")
 
-        # Create a BytesIO object to hold the converted audio data
         buffer = io.BytesIO()
 
-        # Export the converted audio data to the BytesIO object
         audio.export(buffer, format="wav")
 
-        # Load the converted audio data from the BytesIO object
         with sr.AudioFile(buffer) as audio_file:
-            # Adjust for ambient noise
             self._recognizer.adjust_for_ambient_noise(audio_file)
 
-            # Listen to the audio and transcribe
             try:
                 print("Transcribing audio...")
                 audio_data = self._recognizer.record(audio_file)
@@ -39,6 +32,4 @@ class AudioTranscriberSingleton:
                 print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 
-# Example usage:
-audio_transcriber = AudioTranscriberSingleton()
-print(audio_transcriber.transcribe_audio('voice_messages/prueba.ogg'))
+
